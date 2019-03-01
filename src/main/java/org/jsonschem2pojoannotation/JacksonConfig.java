6f472b6761649444f9faa6b928ac49ee87f67ff4
REPLYNET\st.imperiale <st.imperiale@reply.it>
2019-03-01 18:07:11 +0100

    first commit

---
+++ src/main/java/org/jsonschem2pojoannotation/JacksonConfig.java
@@ -0,0 +1,23 @@
+package org.jsonschem2pojoannotation;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JFieldVar;
+import java.time.ZonedDateTime;
+import org.jsonschema2pojo.AbstractAnnotator;
+
+public class JacksonConfig extends AbstractAnnotator {
+    public JacksonConfig() {
+    }
+
+    public void propertyField(JFieldVar field, JDefinedClass clazz, String propertyName, JsonNode propertyNode) {
+        super.propertyField(field, clazz, propertyName, propertyNode);
+        if (field.type().erasure().equals(field.type().owner().ref(ZonedDateTime.class))) {
+            field.annotate(JsonDeserialize.class).param("using", JsonZonedDateTimeJackson.ZonedDateTimeDeserializer.class);
+            field.annotate(JsonSerialize.class).param("using", JsonZonedDateTimeJackson.ZonedDateTimeSerializer.class);
+        }
+
+    }
+}
---
+++ src/main/java/org/jsonschem2pojoannotation/JsonZonedDateTimeJackson.java
@@ -0,0 +1,36 @@
+package org.jsonschem2pojoannotation;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.node.TextNode;
+import java.io.IOException;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class JsonZonedDateTimeJackson {
+    public JsonZonedDateTimeJackson() {
+    }
+
+    public static class ZonedDateTimeSerializer extends JsonSerializer<ZonedDateTime> {
+        public ZonedDateTimeSerializer() {
+        }
+
+        public void serialize(ZonedDateTime value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+            String zdts = value.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+            gen.writeString(zdts);
+        }
+    }
+
+    public static class ZonedDateTimeDeserializer extends JsonDeserializer<ZonedDateTime> {
+        public ZonedDateTimeDeserializer() {
+        }
+
+        public ZonedDateTime deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+            return ZonedDateTime.parse(((TextNode)p.readValueAs(TextNode.class)).textValue(), DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+        }
+    }
+}
